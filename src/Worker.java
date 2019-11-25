80cd6def0cf2f0fef3bbc9c3ef8f66388ed37368
krzysztof <k@example.com>
2019-11-25 13:05:32 +0100

    b2 - c0

--- src/Worker.java
+++ src/Worker.java
@@ -3,4 +3,22 @@ import java.math.BigDecimal;
 public class Worker {
     private String name;
     private BigDecimal salary;
+
+    public Worker() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
 }
